dee0591c052ad2be3afea6cf67ec9312f1b73299
galbadrakh_b <b.galbadrah12@gmail.com>
2020-01-26 17:12:16 -0600

    test

--- js/inheritance.js
+++ js/inheritance.js
@@ -1,5 +1,5 @@
 (function() {
-  //Setting filter function in Stirng object
+  //   Setting filter function in Stirng object
   String.prototype.filter = function(removeingWord) {
     return this.replace(removeingWord, "");
   };
@@ -22,4 +22,47 @@
   };
   let arr = [16, 0, -6, 3, 5, 4, -3, 5, 6, -8, 6];
   console.log(arr.bubbleSort());
+
+  //   Teacher object
+
+  var Person = function() {};
+
+  Person.prototype.initialize = function(name, age) {
+    this.name = name;
+    this.age = age;
+  };
+
+  Person.prototype.describe = function() {
+    return this.name + ", " + this.age + " years old";
+  };
+
+  var gal = new Person();
+  gal.initialize("galaa", 20);
+  //   console.log(gal.describe());
+
+  var test = new Person();
+  test.initialize("test", 19);
+  //   console.log(test.describe());
+
+  var Student = function() {};
+  Student.prototype = new Person();
+  Student.prototype.learn = function(subject) {
+    console.log(this.name + " just learned " + subject);
+  };
+
+  var me = new Student();
+  me.initialize("galaa");
+  me.learn("inheritance");
+
+  var Teacher = function() {};
+
+  Teacher.prototype = new Person();
+  Teacher.prototype.teach = function(subject) {
+    return this.name + " teaches " + subject;
+  };
+
+  var teacher = new Teacher();
+  teacher.initialize("Professor", 32);
+  //   console.log(teacher.describe());
+  console.log(teacher.teach("JS"));
 })();

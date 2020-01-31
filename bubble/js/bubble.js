e8b09b30fd0f19637d669cd83e14dbb15fde3c08
galbadrakh_b <b.galbadrah12@gmail.com>
2020-01-30 20:45:30 -0600

    bubble finish

--- bubble/js/bubble.js
+++ bubble/js/bubble.js
@@ -1,27 +1,13 @@
 var timer = null;
 $(document).ready(function() {
   $("#btnStart").click(start);
-  $("#circle").hide();
-  $("#circle").click(removeCircle);
+  $(".circle").hide();
+  $(".circle").click(removeCircle);
 });
 
-$("#playground").click(function() {
-  var docHeight = $(document).height(),
-    docWidth = $(document).width(),
-    $div = $("#playground"),
-    divWidth = $div.width(),
-    divHeight = $div.height(),
-    heightMax = docHeight - divHeight,
-    widthMax = docWidth - divWidth;
-
-  $div.css({
-    left: Math.floor(Math.random() * widthMax),
-    top: Math.floor(Math.random() * heightMax)
-  });
-});
-
-function removeCircle() {
-  $("#circle").hide();
+function removeCircle(e) {
+  e.style.display = "none";
+  //   e.display = "none";
   clearInterval(timer);
 }
 
@@ -31,17 +17,25 @@ function start() {
   var rate = $("#growRate").val();
   var circles = $("#numberCircles").val();
 
-  $("#circle").height(w);
-  $("#circle").width(w);
-  $("#circle").show();
+  $(".circle").height(w);
+  $(".circle").width(w);
+  //   $(".circle").show();
+
+  for (let i = 0; i < parseInt(circles); i++) {
+    $("#playground").prepend("<div class='circle' id='circle" + i + "'></div>");
+  }
 
   timer = setInterval(function() {
-    var h = $("#circle").height();
-    var w = $("#circle").width();
+    var h = $(".circle").height();
+    var w = $(".circle").width();
     h += parseInt(amt);
     w += parseInt(amt);
-    $("#circle").height(h);
-    $("#circle").width(w);
+    $(".circle").height(h);
+    $(".circle").width(w);
+    $(".circle").click(function() {
+      //   console.log(this.id);
+      removeCircle(this);
+    });
   }, rate);
 }
 
@@ -59,3 +53,18 @@ function draw(x) {
     top: Math.floor(Math.random() * heightMax)
   });
 }
+
+$("#playground").click(function() {
+  var docHeight = $(document).height(),
+    docWidth = $(document).width(),
+    $div = $("#playground"),
+    divWidth = $div.width(),
+    divHeight = $div.height(),
+    heightMax = docHeight - divHeight,
+    widthMax = docWidth - divWidth;
+
+  $div.css({
+    left: Math.floor(Math.random() * widthMax),
+    top: Math.floor(Math.random() * heightMax)
+  });
+});
